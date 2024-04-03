269ab51f634a64cc214ebc0934365204c1dee022
Drew Guarnera <ryonek25@wooster.edu>
2024-04-03 09:27:55 -0400

    Added to Commandnotes and additional software installs

--- JuniorIS/run.py
+++ JuniorIS/run.py
@@ -1,9 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from .polyphony_model import PolyphonyRnnModel
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -15,3 +9,5 @@ def hello():
 
 if __name__ == "__main__":
   app.run(debug=True)
+
+  
